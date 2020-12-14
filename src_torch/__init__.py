acd047abbfd8c5c33227f2210c05b9379942fb67
Ryzhtus <shkunkov.a@yandex.ru>
2020-12-14 23:41:09 +0300

    Initial commit

--- src_torch/__init__.py
+++
@@ -1,6 +0,0 @@
-from src_torch.reader import RuPosReader
-from src_torch.indexer import RuPosIndexer
-from src_torch.dataset import RuPosDataset
-from src_torch.utils import load_embeddings
-from src_torch.model import SimpleTagger
-from src_torch.trainer import Trainer
